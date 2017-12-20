b4658894c553643395db19fed995588dfb37763f
Luke Zhang <luke@lukezhang.ca>
2017-12-20 19:43:08 +0700

    lesson 2 navigation refactoring

---
+++ lesson_index.js
@@ -0,0 +1,65 @@
+// lesson 1
+import datatypes from './1/datatypes.md'
+import contracts from './1/contracts.md'
+import math from './1/math.md'
+import lessonoverview from './1/lessonoverview.md'
+import structs from './1/structs.md'
+import arrays from './1/arrays.md'
+import functions from './1/functions.md'
+import arraysstructs2 from './1/arraysstructs2.md'
+import functions2 from './1/functions2.md'
+import functions3 from './1/functions3.md'
+import keccak256 from './1/keccak256.md'
+import puttingittogether from './1/puttingittogether.md'
+import events from './1/events.md'
+import web3js from './1/web3js.md'
+import lessoncomplete from './1/lessoncomplete.md'
+
+// lesson2
+import overview from './2/0-overview.md'
+// import mappings from './2/1-mappings.md'
+// import msgsender from './2/2-msgsender.md'
+// import require from './2/3-require.md'
+// import inheritance from './2/4-inheritance.md'
+// import _import from './2/5-import.md'
+// import storage from './2/6-storage.md'
+// import eatandmultiply2 from './2/7-eatandmultiply2.md'
+// import internalfunctions from './2/8-internalfunctions.md'
+// import interactingcontracts from './2/9-interactingcontracts.md'
+// import interactingcontracts2 from './2/10-interactingcontracts2.md'
+
+// chapterList is an ordered array of chapters. The order represents the order of the chapters.
+// chapter index will be 1-based and not zero-based. First chapter is 1
+
+export default {
+  1: [
+    lessonoverview,
+    contracts,
+    datatypes,
+    math,
+    structs,
+    arrays,
+    functions,
+    arraysstructs2,
+    functions2,
+    functions3,
+    keccak256,
+    puttingittogether,
+    events,
+    web3js,
+    lessoncomplete,
+  ],
+  2: [
+    overview
+    // mappings,
+    // msgsender,
+    // require,
+    // inheritance,
+    // _import,
+    // storage,
+    // eatandmultiply2,
+    // internalfunctions,
+    // interactingcontracts,
+    // interactingcontracts2
+  ]
+}
