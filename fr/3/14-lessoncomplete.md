fc47f0f99faab837bc304024f648768774416924
pl4ym0re <pl4ym0re@gmail.com>
2018-01-29 21:17:29 -0500

    update lander

--- fr/3/14-lessoncomplete.md
+++
@@ -1,7 +0,0 @@
----
-title: Lesson 3 Complete!
-actions: ['checkAnswer', 'hints']
-material:
-  lessonComplete:
-    answer: 1
----
--- index.ts
+++ index.ts
@@ -2,10 +2,12 @@ import en from './en'
 import zh from './zh'
 import pt from './pt'
 import jp from './jp'
+import fr from './fr'
 
 export default {
   en, // English (source for all translations)
   zh,
   pt,
-  jp
+  jp,
+  fr
 }
