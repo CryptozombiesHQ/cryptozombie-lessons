14fc0bc8edeefa47fa3e526c8e135fdf131664cf
phDooY <david91.05@mail.ru>
2018-03-09 04:04:26 +0300

    ru lesson 4 added missing files

--- ru/index.ts
+++ ru/index.ts
@@ -24,7 +24,7 @@ import l2_overview from './2/00-overview.md'
 import overview from './2/1-overview.md'
 import mappings from './2/2-mappings.md'
 import msgsender from './2/3-msgsender.md'
-import l2_require from './2/4-require.md'
+import require from './2/4-require.md'
 import inheritance from './2/5-inheritance.md'
 import importfiles from './2/6-importfiles.md'
 import storage from './2/7-storage.md'
@@ -38,7 +38,6 @@ import wrappingitup from './2/14-wrappingitup.md'
 import lesson2complete from './2/15-lessoncomplete.md'
 
 // lesson3
-
 import l3_overview from './3/00-overview.md'
 import l3_ch1 from './3/01-externaldependencies.md'
 import l3_ch2 from './3/02-ownable.md'
@@ -54,7 +53,6 @@ import l3_ch11 from './3/11-savinggasstorage.md'
 import l3_ch12 from './3/12-forloops.md'
 import l3_ch13 from './3/13-wrappingitup.md'
 import l3_complete from './3/14-lessoncomplete.md'
-
 
 // chapterList is an ordered array of chapters. The order represents the order of the chapters.
 // chapter index will be 1-based and not zero-based. First chapter is 1
@@ -83,7 +81,7 @@ export default {
     overview,
     mappings,
     msgsender,
-    l2_require,
+    require,
     inheritance,
     importfiles,
     storage,
